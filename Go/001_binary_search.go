2ae94f47f0abef648df9166ef2e2a4a69c8fc266
erick <erick_ic@163.com>
2025-06-02 23:21:29 +0800

    线性遍历

--- Go/001_binary_search.go
+++ Go/001_binary_search.go
@@ -5,17 +5,38 @@ package main
 
 import "fmt"
 
+/*
+为什么不直接用 (left + right) / 2 ？
+这是初学者常见写法：
+	mid := (left + right) / 2
+但当 left 和 right 都很大时，left + right 可能会整型溢出，即超过 int 能表示的最大值，导致程序出错或 panic。
+
+安全写法：避免整型溢出
+	mid := left + (right - left) / 2
+right - left 是两数的差，肯定不会溢出
+再加上 left，整体安全。
+
+mid := left + (right - left) / 2 仍然是中间值，和 (left + right) / 2 在数学意义上是等价的，只是写法上更安全，避免整型溢出。
+*/
+
 func search(nums []int, target int) int {
+	// // 线性遍历 时间复杂度：O(n) ------最坏要看完整个数组
+	// for i, num := range nums {
+	// 	if num == target {
+	// 		return i
+	// 	}
+	// }
+	// return -1
+	// 二分查找 时间复杂度：O(log n)，明显更优。
 	left, right := 0, len(nums)-1
 	for left <= right {
-		middle := (right-left)/2 + left
-		num := nums[middle]
-		if num == target {
-			return middle
-		} else if num > target {
-			right = middle - 1
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
 		} else {
-			left = middle + 1
+			right = mid - 1
 		}
 	}
 	return -1
