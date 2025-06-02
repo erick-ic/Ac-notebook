d9aa2f7ace648c22e4fd9220329528804a3813dc
erick <erick_ic@163.com>
2025-06-02 22:50:41 +0800

    二分查找

---
+++ Go/001_binary_search.go
@@ -0,0 +1,29 @@
+// Difficulty: Easy
+// Status: Done
+// Notes: 二分查找
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		middle := (right-left)/2 + left
+		num := nums[middle]
+		if num == target {
+			return middle
+		} else if num > target {
+			right = middle - 1
+		} else {
+			left = middle + 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	// 给定一个n个元素有序的（升序）整型数组nums和一个目标值target，写一个函数搜索nums中的target，如果目标值存在返回下标，否则返回 -1。
+
+	fmt.Println(search([]int{-1, 0, 3, 5, 9, 12}, 9)) // 4
+	fmt.Println(search([]int{-1, 0, 3, 5, 9, 12}, 2)) // -1
+}
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 <!-- start -->
 
-| # | Title | Difficulty | Language | Status | Notes |
-|---|-------|------------|----------|--------|-------|
-| 001 | Two Sum | Easy | Go | Done | 哈希表 |
+| #   | Title         | Difficulty | Language | Status | Notes    |
+| --- | ------------- | ---------- | -------- | ------ | -------- |
+| 001 | Binary Search | Easy       | Go       | Done   | 二分查找 |
 
 <!-- end -->
